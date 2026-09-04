32e48e8fe0b7bf372095788062a6bd5bb59c518f
Ahsan Habib <habibahsan350@gmail.com>
2026-09-04 21:45:38 +0600

    wifi and bluetooth picker will be added

---
+++ config_dotfiles/config/custom_scripts/bluetooth_menu.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# 1. Add or Remove bluetooth
+# 2. return if no input
+# 3. select something
+# 4. perform action
---
+++ config_dotfiles/config/custom_scripts/wifi_menu.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# 1. Add or Remove wifi
+# 2. return if no input
+# 3. select something
+# 4. apply password for 3 times if yes with pass prompt from fuzzel
+# 5. remove selected wifi if no
